88d98c22d89c7a8ce5078f9587250c871627667f
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-23 18:54:05 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
  counter = 0 
- until array[counter] == value_to_find do 
-   nil 
-   counter +=1 
+ while array[counter]!= value_to_find
+ return nil 
+ counter +=1 
  end 
 end
